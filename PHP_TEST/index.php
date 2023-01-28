add7681a81faf07e5db5551bd92611d23eb1d96f
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2023-01-28 22:08:25 +0900

    郵便番号長さ確認メソッド作成

--- PHP_TEST/index.php
+++ PHP_TEST/index.php
@@ -1,4 +1,16 @@
 <?php
 
-echo 'test';
-phpinfo();
+$postal_code = '123-4567';
+
+function checkedPostalCode($str) {
+    $_postal_code = str_replace('-', '', $str);
+    $length       = strlen($_postal_code);
+
+    if ($length === 7) {
+        return true;
+    }
+
+    return false;
+}
+
+var_dump(checkedPostalCode($postal_code));
